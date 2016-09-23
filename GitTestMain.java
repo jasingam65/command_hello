1a81496175ebe4c19d6aaebb058884aa3ac77b54
jasingam65 <jasingam65@naver.com>
2016-09-23 23:41:52 +0900

    modified by github

--- GitTestMain.java
+++ GitTestMain.java
@@ -1,12 +1,10 @@
-ÀÚ
+//For command line git tutoreal
+//GitHub modification 
+
 public class GitTestMain {
 	public static void main(String[] args) {
 		System.out.println("Git World");
-<<<<<<< HEAD
 		System.out.println("insert by master branch");
-=======
 		System.out.println("hotfix branch");
->>>>>>> hotfix
 	}
 }
-ÀÚ
